03e0b43b04ce4c0446b55c992bedd514c4c471ff
DESKTOP-04C7ID6\NickF <fagundesnicollas@gmail.com>
2021-06-28 16:00:00 -0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <quote-page frase="Passei no Senai krl *-*"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuotePage from './components/QuotePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuotePage
   }
 }
 </script>
---
+++ src/components/QuotePage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="quote">
+    <img alt="Foto de Charles Chaplin" src="../assets/charles-chaplin.jpeg">
+    <br><button class="button">QUOTE</button>
+    <h1>{{ frase }}</h1>
+    <p>- Charles Chaplin</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuotePage',
+  props: {
+    frase: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.button {
+  background-color: #4CAF50;
+  border: 4px solid #4CAF50;
+  color:#ffffff;
+  padding: 15px 32px;
+  text-align: center;
+  font-weight: bold;
+  display: inline-block;
+  font-size: 20px;
+  transition-duration: 0.4s;
+}
+.button:hover {
+  background-color: #ffffff;
+  color: #4CAF50;
+}
+</style>
